c429dccd24b2c77d9538c74c02ece408e9974ec4
XLuffyStory <XLuffyStory@users.noreply.github.com>
2018-04-10 22:09:20 +0800

    add  a Controller Test example

---
+++ SpringBootTestStudy/src/main/java/com/app/SpringBootTestStudyApplication.java
@@ -0,0 +1,12 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestStudyApplication.class, args);
+	}
+}
